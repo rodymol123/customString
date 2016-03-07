79dc725f90009cc65d7fcd2c86a7efa682ad7a59
Rody Molenaar <rody.molenaar@xtrada.nl>
2016-03-07 20:03:49 +0100

    Change example to new filter

--- examples/example2.php
+++ examples/example2.php
@@ -6,7 +6,7 @@ require "../classes/customString.php";
 $string = file_get_contents('string.txt');
 
 $CustomString = new customString($string);
-$CustomString->setFilterWords(array("de", "het", "een", "te"));
+$CustomString->filterWithout(["de", "het", "een", "te"]);
 
 var_dump($CustomString->getFrequentieTable());
 var_dump($CustomString->getUsers());
